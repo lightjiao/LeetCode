960feca6a33b607322adc449587229269e0f6cb1
lightjiao <lightjiao@gmail.com>
2020-03-09 05:30:26 +0800

    :sparkles: (10): Solution
    regular expression matching

---
+++ 0010.regular-expression-matching/main.cpp
@@ -0,0 +1,39 @@
+#include <vector>
+#include <iostream>
+#include "Solution_v2.cpp"
+
+using namespace std;
+
+void testCase(string s, string p, bool expect)
+{
+    int result = (Solution()).isMatch(s, p);
+
+    cout << "expect: " << expect << ", " << "result: " << result;
+
+    if (result == expect)
+    {
+        cout << " ok" << endl;
+    }
+    else
+    {
+        cout << " error" << endl;
+    }
+}
+
+int main()
+{
+    testCase("aa", "a", false);
+    testCase("aa", "aa", true);
+    testCase("aa", "..", true);
+    testCase("aa", "a*", true);
+    testCase("ab", "..", true);
+    testCase("aab", "c*a*b", true);
+    testCase("mississippi", "mis*is*p*.", false);
+    testCase("mississippi", "mis*is*ip*.", true);
+
+    testCase("ab", ".*", true);
+    testCase("ab", ".*c", false);
+
+    testCase("aaa", "a*a", true);
+    return 0;
+}
